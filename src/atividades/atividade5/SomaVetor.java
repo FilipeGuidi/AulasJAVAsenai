c73441ee959a568f6fc69b00106dc9f16cb08541
Filipe.Guidi <filipe.guidi@gmail.com>
2026-07-16 21:37:53 -0300

    Adicionando a aula de hoje

--- src/atividades/atividade5/SomaVetor.java
+++ src/atividades/atividade5/SomaVetor.java
@@ -1,5 +1,13 @@
 package atividades.atividade5;
 
+import java.util.Scanner;
+
 public class SomaVetor {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in)
+
+        int[] numero = new int[5]
+
+    }
 
 }
