e429fa67a6180d63f44473c9095f4cde01cdd963
Filipe.Guidi <filipe.guidi@gmail.com>
2026-07-20 21:48:00 -0300

    Adicionando a aula banco

---
+++ src/aulas/encapsulamento/banco/principal/Main.java
@@ -0,0 +1,17 @@
+package aulas.encapsulamento.banco.principal;
+
+import aulas.encapsulamento.banco.modelo.ContaBancaria;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ContaBancaria minhaConta = new ContaBancaria("Fulano", 123456);
+        minhaConta.setTitular("Felipe");
+        System.out.println(minhaConta.getTitular());
+
+        minhaConta.deposistar(100);
+
+        ContaBancaria pessoa1 = new ContaBancaria("Fulano");
+        ContaBancaria pessoa2 = new ContaBancaria("Robso");
+    }
+}
