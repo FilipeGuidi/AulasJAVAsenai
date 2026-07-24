bb5167e8e190297c461290d9e50c0982a2d43f03
Filipe.Guidi <filipe.guidi@gmail.com>
2026-07-23 21:33:49 -0300

    Adicionado a atividade 9

---
+++ src/aulas/polimorfismo/exemplos/formulario/main.java
@@ -0,0 +1,17 @@
+package aulas.polimorfismo.exemplos.formulario;
+
+public class main {
+    public static void main(String[] args) {
+        Cadastro pessoa1 = new Cadastro("Joao", 22, "Professor", true);
+        Cadastro pessoa2 = new Cadastro("Fulano", 33, true);
+        Cadastro pessoa3 = new Cadastro("Ciclano", true);
+        Cadastro pessoa4 = new Cadastro("Beltrano");
+
+        System.out.println(pessoa1);
+        System.out.println(pessoa2);
+        System.out.println(pessoa3);
+        System.out.println(pessoa4);
+
+    }
+
+}
